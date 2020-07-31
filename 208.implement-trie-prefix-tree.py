8028accd34a0e98b3936a5980d6a39de52e906d5
ocavue <ocavue@gmail.com>
2020-08-01 01:08:27 +0800

    208.implement-trie-prefix-tree.py

--- 208.implement-trie-prefix-tree.py
+++ 208.implement-trie-prefix-tree.py
@@ -1,10 +1,10 @@
 """
 submits:
   - date: 2020-08-01
-    cheating: false
+    cheating: true
     minutes: 9
-labels: []
-comment:
+labels: [trie]
+comment: 这道题考察了 Trie（前缀树/字典树）的数据结构
 """
 
 #
@@ -47,36 +47,86 @@ comment:
 #
 
 # @lc code=start
+
+from collections import defaultdict
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.is_word = False
+
+
 class Trie:
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.search_set = set()
-        self.prefix_set = set()
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
         """
         Inserts a word into the trie.
         """
-        assert len(word) >= 1
-
-        self.search_set.add(word)
-        for i in range(1, len(word) + 1):
-            self.prefix_set.add(word[0:i])
-        self.prefix_set.add(word)
+        curr = self.root
+        for c in word:
+            curr = curr.children[c]
+        curr.is_word = True
 
     def search(self, word: str) -> bool:
         """
         Returns if the word is in the trie.
         """
-        return word in self.search_set
+        curr = self.root
+        for c in word:
+            curr = curr.children.get(c)
+            if curr is None:
+                return False
+        return curr.is_word
 
     def startsWith(self, prefix: str) -> bool:
         """
         Returns if there is any word in the trie that starts with the given prefix.
         """
-        return prefix in self.prefix_set
+        curr = self.root
+        for c in prefix:
+            curr = curr.children.get(c)
+            if curr is None:
+                return False
+        return True
+
+
+# # 使用 set 的实现，虽然能完成题目但是和题目想考察的知识点不匹配
+# class Trie:
+#     def __init__(self):
+#         """
+#         Initialize your data structure here.
+#         """
+#         self.search_set = set()
+#         self.prefix_set = set()
+
+#     def insert(self, word: str) -> None:
+#         """
+#         Inserts a word into the trie.
+#         """
+#         assert len(word) >= 1
+
+#         self.search_set.add(word)
+#         for i in range(1, len(word) + 1):
+#             self.prefix_set.add(word[0:i])
+#         self.prefix_set.add(word)
+
+#     def search(self, word: str) -> bool:
+#         """
+#         Returns if the word is in the trie.
+#         """
+#         return word in self.search_set
+
+#     def startsWith(self, prefix: str) -> bool:
+#         """
+#         Returns if there is any word in the trie that starts with the given prefix.
+#         """
+#         return prefix in self.prefix_set
 
 
 # Your Trie object will be instantiated and called as such:
