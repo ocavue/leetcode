c937faf3456695de0e3b0de5225564bd5560c326
ocavue <ocavue@gmail.com>
2020-09-15 22:23:58 +0800

    update

--- .github/statistics.ts
+++ .github/statistics.ts
@@ -1,12 +1,13 @@
 import jsyaml from 'js-yaml'
 import * as fs from 'fs'
 import path from 'path'
+import _ from 'lodash'
 
 type Lang = "py" | 'go'
 
 interface Header {
-    submits?: {
-        date: number,
+    submits: {
+        date: string,
         cheating: boolean,
         minutes: number,
     }[]
@@ -37,7 +38,7 @@ function parse_header(lang: Lang, file_lines: string[]): Header {
     let header_lines = file_lines.slice(from_line_index + 1, to_line_index)
     let header_text = header_lines.join('\n')
     let raw = jsyaml.load(header_text)
-    let header: Header = {}
+    let header: Record<string, any> = {}
 
     for (let field of ['submits', 'labels', 'comment'] as const) {
         header[field] = raw[field]
@@ -47,7 +48,7 @@ function parse_header(lang: Lang, file_lines: string[]): Header {
         throw Error("unknow field: " + JSON.stringify(raw))
     }
 
-    return header
+    return header as Header
 }
 
 
@@ -68,29 +69,55 @@ function walk_questions() {
     for (let file of files) {
         let matched = /^([0-9]+)\..+\.(py|go)$/.exec(file)
         if (matched) {
-            console.log('parsing', file)
-
-            let id = Number(matched[1])
-            let lang = matched[2] as Lang
-            let text = fs.readFileSync(file, { encoding: 'utf-8' })
-            let lines = text.split('\n')
-            questions.push(
-                {
-                    id,
-                    lang,
-                    header: parse_header(lang, lines),
-                }
-            )
+            try {
+                let id = Number(matched[1])
+                let lang = matched[2] as Lang
+                let text = fs.readFileSync(file, { encoding: 'utf-8' })
+                let lines = text.split('\n')
+                questions.push(
+                    {
+                        id,
+                        lang,
+                        header: parse_header(lang, lines),
+                    }
+                )
+            } catch (error) {
+                console.error('failed to parsing', file)
+                throw error
+            }
         }
-
     }
 
-    return JSON.stringify(questions, null, "  ")
+    return questions
+}
+
+function getSorkKey(question: Question): number {
+    let submits = question.header.submits
+    let lastSubmit = submits[submits.length - 1]
+
+    // 没做出来的排在前面
+    // 提交比较早的排在前面
+    return (
+        lastSubmit.cheating ? 0 : Date.parse('2099-01-01T01:01:01') +
+        Date.parse(lastSubmit.date)
+    )
+}
+
+
+function sort_questions(questions: Question[]) {
+    return _.sortBy(
+        questions,
+        getSorkKey
+    )
 }
 
 function main() {
     console.log(
-        walk_questions()
+        JSON.stringify(
+            sort_questions(walk_questions()),
+            null,
+            "  ",
+        )
     )
 }
 
