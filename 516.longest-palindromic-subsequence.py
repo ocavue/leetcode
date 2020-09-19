6bad51809a2277d5dde5a89ee984558152f5357a
ocavue <ocavue@gmail.com>
2020-09-19 19:17:34 +0800

    516.longest-palindromic-subsequence.py

--- 516.longest-palindromic-subsequence.py
+++ 516.longest-palindromic-subsequence.py
@@ -3,6 +3,9 @@ submits:
   - date: 2020-07-26
     cheating: true
     minutes: 120
+  - date: 2020-09-19
+    cheating: true
+    minutes: 23
 comment: |
   这道题重点是构造出状态转移函数。
   状态：以 dp[i][j] 表示 s[i:j+1] 可以找到的 Longest Palindromic Subsequence
@@ -10,6 +13,8 @@ comment: |
         如果 s[i] == s[j]，那么最长回数长度就是 2 + 子串长度
         如果 s[i] != s[j]，那么最长回数长度就是 max(dp[i][j-1] + dp[i+1][j])
   同时要注意这道题和 5.longest-palindromic-substring 是不一样的。subsequence 中的元素是可以离散的，但是 substring 中的元素是紧挨着的。
+
+  这道题也告诉我，在 Leetcode 里，达到 1000*1000 数量级的 DP cache 是可以接受的
 labels: [dp]
 
 """
@@ -77,41 +82,31 @@ from typing import List
 
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        if len(s) == 0:
-            return 0
-        if len(s) == 1:
-            return 1
-        if len(s) == 2:
-            return 2 if s[0] == s[1] else 1
+        # dp[i][j] 表示 s[i:j+1] 中最长的 palindromic subsequence 是多少
 
-        # dp 是一个二维数组，dp[i][j] 表示从 s 的第 i 项到第 j 项，Longest Palindromic Subsequence 的长度是多少
-        # 其中 0 <= i <= j < len(s)
         dp: List[List[int]] = []
         for _ in range(len(s)):
-            dp.append([0] * len(s))
-        # assert len(dp) == len(dp[0]) == len(s)
+            dp.append([0] * (len(s)))
 
-        # 当 i == j 的时候
-        for i in range(len(s)):
-            dp[i][i] = 1
+        def cal(i: int, j: int):
+            assert 0 <= i <= j < len(s)
 
-        # 当 j-i == 1 的时候
-        for j in range(1, len(s)):
-            i = j - 1
-            dp[i][j] = 2 if s[i] == s[j] else 1
+            if i == j:
+                return 1
+            if i == j - 1:
+                return 2 if s[i] == s[j] else 1
 
-        # 当 l = j-i >= 2 的时候
-        for l in range(2, len(s)):
-            for j in range(2, len(s)):
-                i = j - l
+            if s[i] == s[j]:
+                return 2 + cal_with_cache(i + 1, j - 1)
+            else:
+                return max(cal_with_cache(i + 1, j), cal_with_cache(i, j - 1))
 
-                ############ 状态转移 ############
-                if s[i] == s[j]:
-                    dp[i][j] = 2 + dp[i + 1][j - 1]
-                else:
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        def cal_with_cache(i: int, j: int):
+            if dp[i][j] == 0:
+                dp[i][j] = cal(i, j)
+            return dp[i][j]
 
-        return dp[0][len(s) - 1]
+        return cal_with_cache(0, len(s) - 1)
 
 
 # @lc code=end
